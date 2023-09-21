9a919c623947ac983e208b7a20ad0ee1f7aad932
Neil Fisher <neil3524@gmail.com>
2023-09-21 01:38:59 -0400

    Remove project name fallback

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.BASE_PATH || getProjectName(),
+  basePath: process.env.BASE_PATH,
 };
-
-function getProjectName() {
-  const { execSync } = require("child_process");
-  const projectName = execSync(`git remote -v | sed -rn '1s#.*/(.*)\\.git.*#\\1#p'`)
-    .toString()
-    .trim();
-  console.log(`Project name: '${projectName}'`);
-  if (projectName) {
-    return "/" + projectName;
-  }
-}
 
 module.exports = nextConfig;
