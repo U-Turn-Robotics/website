d260baeab73aaf82a5a907cd3ed3653ec4df84da
Neil Fisher <neil3524@gmail.com>
2023-09-21 01:29:53 -0400

    Fix basePath for pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.BASE_PATH || getProjectName(),
 };
+
+function getProjectName() {
+  const { execSync } = require("child_process");
+  const projectName = execSync(`git remote -v | sed -rn '1s#.*/(.*)\\.git.*#\\1#p'`)
+    .toString()
+    .trim();
+  console.log(`Project name: '${projectName}'`);
+  if (projectName) {
+    return "/" + projectName;
+  }
+}
 
 module.exports = nextConfig;
