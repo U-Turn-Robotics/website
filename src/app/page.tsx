f6b449ffbbdaa5e79d6c8bdc3d252920b9f6665d
Neil Fisher <neil3524@gmail.com>
2023-09-21 02:46:15 -0400

    Add glow to logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,16 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-between py-28">
       <img
-        src={"U-Turn_Robotics-2.svg"}
+        src={"U-Turn_Robotics-transparent.svg"}
         width={500}
         height={500}
-        className="mix-blend-difference"
+        className="w-[70vw] max-w-[35em] 
+        transition-[filter] duration-500 
+        [--logo-drop-shadow-o:0.2] 
+        [filter:drop-shadow(0_0_22px_rgb(var(--background-end-rgb),var(--logo-drop-shadow-o)))] 
+        hover:[--logo-drop-shadow-o:0.4] active:[--logo-drop-shadow-o:0.4]"
       />
       <h1 className="text-4xl font-medium">U-Turn Robotics</h1>
       <p className="text-3xl">
